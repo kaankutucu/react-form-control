3fe2ad5a983d8179db8be3e42b4e3f592dc3d2ba
Kaan <49414441+kaankutucu@users.noreply.github.com>
2022-04-10 09:46:32 +0300

    radio, checkbox, dropdown

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
             } else if (
                 !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
             ) {
-              errors.email = 'Invalid email address';
+              errors.email = 'Invalid email addresss';
             }
             return errors;
           }}
